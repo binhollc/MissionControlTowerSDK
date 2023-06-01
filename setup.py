89c2e34cd26e72c65a430190a71367ac65a2ed32
Gustavo Armagno <gustavoa@gmail.com>
2023-06-01 18:09:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ executables = [Executable(script=SUBMODULE_FOLDER + "/bridge.py")]
 # Set up the freezing options
 options = {
     "build_exe": {
-        "include_files": [(SUBMODULE_FOLDER + "/mock_command_adaptor.py", "mock_command_adaptor.py"), (SUBMODULE_FOLDER + "/mock_host_adapter_sdk.py", "mock_host_adapter_sdk.py")],
+        "build_exe": "build_bridge",
         "path": sys.path + [os.path.abspath(os.path.join(os.path.dirname(__file__), "mission-control-tower")), os.path.abspath(os.path.join(os.path.dirname(__file__), "python-backend"))],
         "excludes": ["tkinter"]
     }
