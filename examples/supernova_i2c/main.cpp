724ce9cc5c3ac0be937df383262330a7c312808d
Gustavo Armagno <gustavoa@gmail.com>
2023-12-20 17:09:47 -0300

    Enhance I2C example

--- examples/supernova_i2c/main.cpp
+++ examples/supernova_i2c/main.cpp
@@ -1,18 +1,13 @@
 #include "CommandDispatcher.h"
 #include <iostream>
 
-void process_response(const CommandResponse& cr) {
-    if (cr.status == "success") {
-        if (cr.data.find("data") != cr.data.end()) {
-          if (cr.data["data"].is_array()) {  // Check if the data is an array
-              for (const auto& byte : cr.data["data"]) {
-                  std::cout << byte.template get<std::string>() << " ";
-              }
-          }
-        }
-    } else {
-        std::cout << "Error occurred.\n";
-    }
+void printCommandResponse(const CommandResponse& cr, const std::string& action) {
+    std::cout << "Action: " << action << "\n";
+    std::cout << "Transaction ID: " << cr.transaction_id << "\n";
+    std::cout << "Status: " << cr.status << "\n";
+    std::cout << "Is Promise: " << (cr.is_promise ? "True" : "False") << "\n";
+    std::cout << "Data: " << cr.data.dump() << "\n";
+    std::cout << "----------------------------------\n";
 }
 
 int main() {
@@ -21,33 +16,48 @@ int main() {
     dispatcher.start();
 
     // Open device
-    dispatcher.invokeCommandSync("0", "open", {}, process_response);
+    dispatcher.invokeCommandSync("0", "open", {}, [](CommandResponse cr) {
+        printCommandResponse(cr, "Opening Supernova");
+    });
 
     // Set bus voltage
-    dispatcher.invokeCommandSync("0", "i2c_spi_uart_set_bus_voltage", {{"busVoltageInV", "3.3"}}, process_response);
+    dispatcher.invokeCommandSync("0", "i2c_spi_uart_set_bus_voltage", {{"busVoltageInV", "3.3"}}, [](CommandResponse cr) {
+        printCommandResponse(cr, "Setting Bus Voltage");
+    });
 
     // Set I2C parameters
-    dispatcher.invokeCommandSync("0", "i2c_set_parameters", {{"clockFrequencyInKHz", "400"}}, process_response);
+    dispatcher.invokeCommandSync("0", "i2c_set_parameters", {{"clockFrequencyInKHz", "400"}}, [](CommandResponse cr) {
+        printCommandResponse(cr, "Setting Clock Frequency");
+    });
 
-    // Write to I2C address
-    dispatcher.invokeCommandSync("0", "i2c_write", {{"address", "50"}, {"writeBuffer", "AABB"}}, process_response);
+    // Write 10 zeros to subaddress 0000
+    dispatcher.invokeCommandSync("0", "i2c_write", {{"address", "50"}, {"writeBuffer", "000000000000000000000000"}}, [](CommandResponse cr) {
+        printCommandResponse(cr, "Write 10 zeros to subaddress 0000");
+    });
 
-    // Read from I2C address
-    dispatcher.invokeCommandSync("0", "i2c_read", {{"address", "50"}, {"bytesToRead", "17"}}, process_response);
+    // Read 10 bytes from subaddress 0000
+    dispatcher.invokeCommandSync("0", "i2c_write", {{"address", "50"}, {"writeBuffer", "0000"}}, [](CommandResponse cr) {});
+    dispatcher.invokeCommandSync("0", "i2c_read", {{"address", "50"}, {"bytesToRead", "10"}}, [](CommandResponse cr) {
+        printCommandResponse(cr, "Reading 10 bytes from subaddress 0000");
+    });
 
     // Write to I2C address using subaddress
     dispatcher.invokeCommandSync("0", "i2c_write_using_subaddress", {
         {"address", "50"},
-        {"subaddress", "0000"},
-        {"writeBuffer", "010203040506"}
-    }, process_response);
+        {"subaddress", "0005"},
+        {"writeBuffer", "0102030405"}
+    }, [](CommandResponse cr) {
+        printCommandResponse(cr, "Write [01,02,03,04,05] to subaddress 0005");
+    });
 
     // Read from I2C address using subaddress
     dispatcher.invokeCommandSync("0", "i2c_read_using_subaddress", {
         {"address", "50"},
         {"subaddress", "0000"},
         {"bytesToRead", "10"}
-    }, process_response);
+    }, [](CommandResponse cr) {
+        printCommandResponse(cr, "Read 10 bytes from subaddress 0000");
+    });
 
     // Exit
     dispatcher.invokeCommandSync("0", "exit", {});
