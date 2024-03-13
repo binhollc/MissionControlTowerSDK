0e461160edd806cde76609a1fe5ec9e3649991dd
Gustavo Armagno <gustavoa@gmail.com>
2024-03-13 12:50:00 -0300

    987|Refactor test name

---
+++ tests/CommandDispatcherGenericFlowTest.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include "CommandDispatcher.h"
+#include <nlohmann/json.hpp>
+#include <chrono>
+#include <thread>
+
+// Global constant for minimum tolerable waiting time for a command
+constexpr std::chrono::milliseconds MIN_TOLERABLE_CMD_WAIT_MS(300);
+
+class GenericCommandFlowTest : public ::testing::Test {
+protected:
+    CommandDispatcher dispatcher;
+
+    GenericCommandFlowTest() : dispatcher("BinhoSupernova") {}
+
+    void SetUp() override {
+      dispatcher.start();
+    }
+
+    void TearDown() override {
+      dispatcher.waitForAllCommands();
+      dispatcher.stop();
+    }
+};
+
+TEST_F(GenericCommandFlowTest, OpenCloseExit_SimulatedPort_OperationsCompleteSuccessfully) {
+    bool openInvoked = false;
+    bool closeInvoked = false;
+    bool exitInvoked = false;
+
+    // Open the SupernovaSimulatedPort
+    dispatcher.invokeCommand("1", "open", {{"address", "SupernovaSimulatedPort"}}, [&](CommandResponse cr) {
+      openInvoked = true;
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(MIN_TOLERABLE_CMD_WAIT_MS));
+
+    EXPECT_TRUE(openInvoked);
+
+    dispatcher.waitFor({"1"});
+
+    // TODO: Uncomment the code below when https://focusuy.atlassian.net/browse/BMC2-991 is solved:
+
+    // dispatcher.invokeCommand("2", "close", {}, [&](CommandResponse cr) {
+    //   closeInvoked = true;
+    // });
+
+    // std::this_thread::sleep_for(std::chrono::milliseconds(MIN_TOLERABLE_CMD_WAIT_MS));
+
+    // EXPECT_TRUE(closeInvoked);
+
+    dispatcher.invokeCommand("1000", "exit", {}, [&](CommandResponse cr) {
+      exitInvoked = true;
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(MIN_TOLERABLE_CMD_WAIT_MS));
+
+    EXPECT_TRUE(exitInvoked);
+
+    dispatcher.waitFor({"1000"});
+}
