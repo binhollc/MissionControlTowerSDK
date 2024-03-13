090cf30da373f42a22ab99400a696b501f9bb874
Gustavo Armagno <gustavoa@gmail.com>
2024-03-13 17:36:00 -0300

    993|Refactor to add an ID generator

--- examples/i3c_ibis/main.cpp
+++ examples/i3c_ibis/main.cpp
@@ -13,6 +13,18 @@ void printCommandResponse(const CommandResponse &cr, const std::string &action)
   std::cout << "----------------------------------\n";
 }
 
+class CommandIDGenerator {
+public:
+    CommandIDGenerator() : currentID(0) {}
+
+    std::string nextID() {
+        return std::to_string(++currentID);
+    }
+
+private:
+    unsigned long long currentID;
+};
+
 // Utility function to simplify command invocation
 void invokeI3CWriteUsingSubaddress(CommandDispatcher& dispatcher, const std::string& transactionId,
                                    const std::string& subaddress, const std::string& writeBuffer,
@@ -21,51 +33,53 @@ void invokeI3CWriteUsingSubaddress(CommandDispatcher& dispatcher, const std::str
     dispatcher.invokeCommandSync(transactionId, "i3c_write_using_subaddress", {{"address", address}, {"subaddress", subaddress}, {"writeBuffer", writeBuffer}, {"mode", mode}, {"pushPullClockFrequencyInMHz", pushPullClockFrequencyInMHz}, {"openDrainClockFrequencyInKHz", openDrainClockFrequencyInKHz}});
 }
 
+
 int main()
 {
   CommandDispatcher dispatcher("BinhoSupernova");
+  CommandIDGenerator idGenerator;
 
   dispatcher.start();
 
   dispatcher.onNotification([&](const CommandResponse &cr) {
-    if (cr.transaction_id == "0") {
-        printCommandResponse(cr, "IBI RECEIVED");
-    }
+      if (cr.transaction_id == "0") {
+          printCommandResponse(cr, "IBI RECEIVED");
+      }
   });
 
   // Open device
-  dispatcher.invokeCommandSync("1", "open", {}, [](CommandResponse cr) {
+  dispatcher.invokeCommandSync(idGenerator.nextID(), "open", {}, [](CommandResponse cr) {
       printCommandResponse(cr, "Supernova Opened");
   });
 
   // Set bus voltage
-  dispatcher.invokeCommandSync("2", "i3c_init_bus", {{"busVoltageInV", "3.3"}}, [](CommandResponse cr) {
+  dispatcher.invokeCommandSync(idGenerator.nextID(), "i3c_init_bus", {{"busVoltageInV", "3.3"}}, [](CommandResponse cr) {
       printCommandResponse(cr, "Bus Voltage Set");
   });
 
   // Command to enable IBIs on the target device
-  dispatcher.invokeCommandSync("100", "i3c_ibi_enable", {{"address","08"}});
+  dispatcher.invokeCommandSync(idGenerator.nextID(), "i3c_ibi_enable", {{"address", "08"}});
 
-  // Refactored commands to write using subaddress
-  invokeI3CWriteUsingSubaddress(dispatcher, "3", "76", "00");
-  invokeI3CWriteUsingSubaddress(dispatcher, "4", "4E", "20");
-  invokeI3CWriteUsingSubaddress(dispatcher, "5", "13", "05");
-  invokeI3CWriteUsingSubaddress(dispatcher, "6", "16", "40");
-  invokeI3CWriteUsingSubaddress(dispatcher, "7", "5F", "61");
-  invokeI3CWriteUsingSubaddress(dispatcher, "8", "60", "0F00");
-  invokeI3CWriteUsingSubaddress(dispatcher, "9", "50", "0E");
-  invokeI3CWriteUsingSubaddress(dispatcher, "10", "76", "01");
-  invokeI3CWriteUsingSubaddress(dispatcher, "11", "03", "38");
-  invokeI3CWriteUsingSubaddress(dispatcher, "12", "7A", "02");
-  invokeI3CWriteUsingSubaddress(dispatcher, "13", "7C", "1F");
-  invokeI3CWriteUsingSubaddress(dispatcher, "14", "76", "04");
-  invokeI3CWriteUsingSubaddress(dispatcher, "15", "4F", "04");
-  invokeI3CWriteUsingSubaddress(dispatcher, "16", "76", "00");
-  invokeI3CWriteUsingSubaddress(dispatcher, "17", "4E", "02");
+  // Refactored commands to write using subaddress with auto-generated IDs
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "76", "00");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "4E", "20");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "13", "05");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "16", "40");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "5F", "61");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "60", "0F00");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "50", "0E");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "76", "01");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "03", "38");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "7A", "02");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "7C", "1F");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "76", "04");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "4F", "04");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "76", "00");
+  invokeI3CWriteUsingSubaddress(dispatcher, idGenerator.nextID(), "4E", "02");
 
   std::this_thread::sleep_for(std::chrono::milliseconds(10000));
 
-  dispatcher.invokeCommandSync("1000", "exit", {});
+  dispatcher.invokeCommandSync(idGenerator.nextID(), "exit", {});
 
   dispatcher.waitForAllCommands();
 
