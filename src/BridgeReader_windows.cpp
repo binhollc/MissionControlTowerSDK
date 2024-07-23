3291faa63b8d1b53e8201debbe9e717cb9f514c6
Gustavo Armagno <gustavoa@gmail.com>
2024-07-23 13:56:36 -0300

    1056 | Remove commented code block

--- src/BridgeReader_windows.cpp
+++ src/BridgeReader_windows.cpp
@@ -37,31 +37,3 @@ std::string BridgeReader::readNextData() {
         }
     }
 }
-
-// std::string BridgeReader::readNextData() {
-//     if (bridgeProcessRead == NULL) {
-//         return "__EOF__";
-//     }
-
-//     const int bufferSize = 1024;
-//     DWORD bytesRead;
-//     char buffer[bufferSize];
-//     std::vector<char> line;
-
-//     while (true) {
-//         if (!ReadFile(bridgeProcessRead, buffer, bufferSize - 1, &bytesRead, NULL) || bytesRead == 0) {
-//             // EOF or error
-//             return "__EOF__";
-//         }
-
-//         buffer[bytesRead] = '\0'; // Null-terminate what we've read
-//         line.insert(line.end(), buffer, buffer + bytesRead);
-
-//         // Check if we have a full line
-//         auto it = std::find(line.begin(), line.end(), '\n');
-//         if (it != line.end()) {
-//             *it = '\0'; // Replace newline with null-terminator
-//             return std::string(line.data());
-//         }
-//     }
-// }
