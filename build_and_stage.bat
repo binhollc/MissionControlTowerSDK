f11a4b0738b63164e9a3fdf2011c2bf991b3ebc7
Gustavo Armagno <gustavoa@gmail.com>
2024-07-30 12:17:47 -0300

    Remove download step from Windows script

--- build_and_stage.bat
+++ build_and_stage.bat
@@ -15,49 +15,11 @@ if not "%~1"=="" set "PLATFORM=%~1"
 if exist "%STAGING_DIR%" rmdir /S /Q "%STAGING_DIR%"
 mkdir "%STAGING_DIR%"
 
-echo :: ---
-echo Installing the bridge...
-echo :: ---
-
-:: Remove the build_bridge directory
-if exist "%BRIDGE_BUILD_DIR%" rmdir /S /Q "%BRIDGE_BUILD_DIR%"
-
-:: Download the bridge depending on the platform
-echo Downloading the bridge...
-if "%PLATFORM%"=="win32" (
-    curl -L https://cdn.binho.io/sw/MissionControlBridge/0.12.0/windows-latest-32-artifacts.zip -o "bridge.zip"
-) else (
-    curl -L https://cdn.binho.io/sw/MissionControlBridge/0.12.0/windows-latest-64-artifacts.zip -o "bridge.zip"
+:: TODO: Check for existence of bridge.exe in the PATH, if not warn 'Bridge executable could not be found. Please check README.md'
+where bridge.exe >nul 2>nul
+if errorlevel 1 (
+    echo WARNING: Bridge executable could not be found. Please check README.md
 )
-
-:: Extract the bridge
-echo Extracting the bridge...
-tar -xf bridge.zip
-
-:: Remove the bridge zip
-del bridge.zip
-
-:: Rename the directory from bridge to build_bridge
-ren bridge "%BRIDGE_BUILD_DIR%"
-
-:: Check if the bridge was downloaded and extracted successfully
-if not exist "%BRIDGE_BUILD_DIR%" (
-    echo Bridge download and extraction failed
-    exit /b 1
-)
-if not exist "%BRIDGE_BUILD_DIR%\bridge.exe" (
-    echo Bridge download and extraction failed
-    exit /b 1
-)
-
-echo :: ---
-echo Staging the Bridge...
-echo :: ---
-
-echo Copying build_bridge to staging...
-echo "%STAGING_DIR%\bridge"
-rmdir /S /Q "%STAGING_DIR%\bridge"
-xcopy /E /I /Y build_bridge "%STAGING_DIR%\bridge"
 
 echo :: ---
 echo Building and staging bmc_sdk library, docs and examples...
