39a9b7906ad34e15d7d257f94405bb2b9a244851
Gustavo Armagno <gustavoa@gmail.com>
2024-07-30 12:04:21 -0300

    1050 | Remove download step from macOS script

--- build_and_stage.sh
+++ build_and_stage.sh
@@ -20,59 +20,20 @@ print_colored_message() {
 # Set the PROJECT_DIR variable.
 PROJECT_DIR="$(pwd)"
 
-# Build bridge
-print_colored_message "${INFO}" "Building the Bridge"
-
-# Clean the build_bridge directory
-if [ -d "build_bridge" ]; then
-    rm -rf build_bridge
+# Ensure that the bridge executable is accessible
+if ! command -v bridge &> /dev/null
+then
+    print_colored_message "${WARNING}" "Bridge executable could not be found. Please check README.md"
 fi
-
-# Download the bridge for the current OS
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    print_colored_message "${INFO}" "Downloading the bridge for MacOS"
-    curl -L -o bridge.zip https://cdn.binho.io/sw/MissionControlBridge/0.12.0/macos-latest-64-artifacts.zip
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    print_colored_message "${INFO}" "Downloading the bridge for Ubuntu"
-    curl -L -o bridge.zip https://cdn.binho.io/sw/MissionControlBridge/0.12.0/ubuntu-latest-64-artifacts.zip
-else
-    print_colored_message "${ERROR}" "Error: Unsupported OS"
-    exit 1
-fi
-
-# Unzip the bridge
-unzip bridge.zip
-
-# Remove the zip file
-rm bridge.zip
-
-# Rename the directory from bridge to build_bridge
-mv bridge build_bridge
-
-# Check if build_bridge was created with a file named bridge within. If not, exit with an error.
-if [ ! -d "build_bridge" ] || [ ! -f "build_bridge/bridge" ]; then
-    print_colored_message "${ERROR}" "Error: The bridge was not downloaded successfully"
-    exit 1
-fi
-
 
 # Clean the staging directory
-
 if [ -d "staging" ]; then
     rm -rf staging
 fi
 
 mkdir staging
 
-# Stage the bridge
-
-print_colored_message "${INFO}" "Staging the bridge"
-
-cd "$PROJECT_DIR"
-cp -R build_bridge staging/bridge
-
 # Build library and examples
-
 print_colored_message "${INFO}" "Building the bmc_sdk library and examples"
 
 cd "$PROJECT_DIR"
@@ -87,7 +48,6 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release --target install
 
 # Stage library, docs and examples
-
 print_colored_message "${SUCCESS}" "All done!"
 
 cd "$PROJECT_DIR"
