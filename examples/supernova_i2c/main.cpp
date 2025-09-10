409ad8ce624a7101f0a8e1e1525cc9b1926311f5
Gustavo Armagno <gustavoa@gmail.com>
2025-09-10 12:25:50 -0300

    2340 | Migrate supernova_i2c example to the Bridge 1.3 API

--- examples/supernova_i2c/main.cpp
+++ examples/supernova_i2c/main.cpp
@@ -16,34 +16,37 @@ int main() {
     dispatcher.start();
 
     // Open device
-    dispatcher.invokeCommandSync("0", "open", {}, [](CommandResponse cr) {
+    dispatcher.invokeCommandSync("1", "open", {}, [](CommandResponse cr) {
         printCommandResponse(cr, "Opening Supernova");
     });
 
+    // Initialize I2C controller
+    dispatcher.invokeCommandSync("1", "i2c_controller_init", {
+        {"clockFrequencyInKHz", 400},
+        {"pullUpResistanceInOhm", "DISABLE"}
+    }, [](CommandResponse cr) {
+        printCommandResponse(cr, "I2C controller initialized");
+    });
+
     // Set bus voltage
-    dispatcher.invokeCommandSync("0", "i2c_spi_uart_set_bus_voltage", {{"busVoltageInV", "3.3"}}, [](CommandResponse cr) {
+    dispatcher.invokeCommandSync("1", "i2c_spi_uart_set_bus_voltage", {{"busVoltageInV", "3.3"}}, [](CommandResponse cr) {
         printCommandResponse(cr, "Setting Bus Voltage");
     });
 
-    // Set I2C parameters
-    dispatcher.invokeCommandSync("0", "i2c_set_parameters", {{"clockFrequencyInKHz", "400"}}, [](CommandResponse cr) {
-        printCommandResponse(cr, "Setting Clock Frequency");
-    });
-
     // Write 10 zeros to subaddress 0000
-    dispatcher.invokeCommandSync("0", "i2c_write", {{"address", "50"}, {"writeBuffer", "000000000000000000000000"}}, [](CommandResponse cr) {
+    dispatcher.invokeCommandSync("1", "i2c_write", {{"address", 0x50}, {"writeBuffer", "000000000000000000000000"}}, [](CommandResponse cr) {
         printCommandResponse(cr, "Write 10 zeros to subaddress 0000");
     });
 
     // Read 10 bytes from subaddress 0000
-    dispatcher.invokeCommandSync("0", "i2c_write", {{"address", "50"}, {"writeBuffer", "0000"}}, [](CommandResponse cr) {});
-    dispatcher.invokeCommandSync("0", "i2c_read", {{"address", "50"}, {"bytesToRead", "10"}}, [](CommandResponse cr) {
+    dispatcher.invokeCommandSync("1", "i2c_write", {{"address", 0x50}, {"writeBuffer", "0000"}}, [](CommandResponse cr) {});
+    dispatcher.invokeCommandSync("1", "i2c_read", {{"address", 0x50}, {"bytesToRead", "10"}}, [](CommandResponse cr) {
         printCommandResponse(cr, "Reading 10 bytes from subaddress 0000");
     });
 
     // Write to I2C address using subaddress
-    dispatcher.invokeCommandSync("0", "i2c_write_using_subaddress", {
-        {"address", "50"},
+    dispatcher.invokeCommandSync("1", "i2c_write_using_subaddress", {
+        {"address", 0x50},
         {"subaddress", "0005"},
         {"writeBuffer", "0102030405"}
     }, [](CommandResponse cr) {
@@ -51,16 +54,19 @@ int main() {
     });
 
     // Read from I2C address using subaddress
-    dispatcher.invokeCommandSync("0", "i2c_read_using_subaddress", {
-        {"address", "50"},
+    dispatcher.invokeCommandSync("1", "i2c_read_using_subaddress", {
+        {"address", 0x50},
         {"subaddress", "0000"},
         {"bytesToRead", "10"}
     }, [](CommandResponse cr) {
         printCommandResponse(cr, "Read 10 bytes from subaddress 0000");
     });
 
+    // Close device
+    dispatcher.invokeCommandSync("1", "close", {});
+
     // Exit
-    dispatcher.invokeCommandSync("0", "exit", {});
+    dispatcher.invokeCommandSync("1", "exit", {});
 
     dispatcher.stop();
 
